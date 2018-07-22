4ec4c3a81552c96fb0fe1ddf9b0b93071e04b9b9
eihsan94 <eihsan94@gmail.com>
2018-07-22 17:32:27 +0900

    set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
+// this file tells angular how to assemble the app
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
