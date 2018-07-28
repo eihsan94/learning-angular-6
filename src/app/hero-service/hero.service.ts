7496f8b18cb8bf1a90dcd08d08144f4b46447573
eihsan94 <eihsan94@gmail.com>
2018-07-28 17:37:37 +0900

    adding the message component and service

--- src/app/hero-service/hero.service.ts
+++ src/app/hero-service/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { Observable, of } from 'rxjs';
-
+import { MessageService } from '../message/message.service';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { } // injecting the message service
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
---
+++ src/app/message/README.md
@@ -0,0 +1,6 @@
+
+# the thing that we did in this branch
+* we make a message service that show the message when the message is retrieved successfully from the server
+
+# new things that we learn
+* how to make the notification stating the data traffic condition from the server
